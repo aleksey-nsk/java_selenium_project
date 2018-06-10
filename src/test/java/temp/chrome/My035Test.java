1667776cef9b731d95b3b8e943a834800af29f12
Alex Z <aleksey.zhd@gmail.com>
2018-06-10 23:43:01 +0700

    создал класс 035 на тему XPath

--- src/test/java/temp/chrome/My035Test.java
+++ src/test/java/temp/chrome/My035Test.java
@@ -1,3 +1,5 @@
+// Перепишу класс My001Test с использованием XPath-запросов
+
 package temp.chrome;
 
 import org.junit.AfterClass;
@@ -10,7 +12,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
-public class My001Test {
+public class My035Test {
     private static WebDriver driver;
 
     @BeforeClass
@@ -25,19 +27,23 @@ public class My001Test {
     @Test
     public void action(){
         // Ввожу логин:
-        WebElement loginField = driver.findElement(By.id("login"));
+        // WebElement loginField = driver.findElement(By.id("login"));
+        WebElement loginField = driver.findElement(By.xpath("//input[@id='login']"));
         loginField.sendKeys("user.testov");
 
         // Ввожу пароль:
-        WebElement passwordField = driver.findElement(By.id("pass"));
+        // WebElement passwordField = driver.findElement(By.id("pass"));
+        WebElement passwordField = driver.findElement(By.xpath("//input[@id='pass']"));
         passwordField.sendKeys("zxc67*Q");
 
         // Жму кнопку Войти:
-        WebElement loginButton = driver.findElement(By.cssSelector(".ngsmail__login-submit"));
+        // WebElement loginButton = driver.findElement(By.cssSelector(".ngsmail__login-submit"));
+        WebElement loginButton = driver.findElement(By.xpath("//button[contains(@class,'ngsmail__login-submit')]"));
         loginButton.click();
 
         // Убеждаюсь что попал в свой почтовый ящик:
-        WebElement profileUser = driver.findElement(By.id("td_header_right1"));
+        // WebElement profileUser = driver.findElement(By.id("td_header_right1"));
+        WebElement profileUser = driver.findElement(By.xpath("//td[@id='td_header_right1']"));
         String mailUser = profileUser.getText();
         Assert.assertEquals("user.testov@ngs.ruВыход", mailUser);
     }
@@ -47,7 +53,8 @@ public class My001Test {
         // Нажать кнопку Выйти:
         // WebElement logoutButton = driver.findElement(By.cssSelector("a[href='logout']"));
         // WebElement logoutButton = driver.findElement(By.cssSelector("[href=logout]"));
-        WebElement logoutButton = driver.findElement(By.cssSelector("a[href=logout]"));
+        // WebElement logoutButton = driver.findElement(By.cssSelector("a[href=logout]"));
+        WebElement logoutButton = driver.findElement(By.xpath("//a[@href='logout']"));
         logoutButton.click();
 
         // Закрыть браузер:
