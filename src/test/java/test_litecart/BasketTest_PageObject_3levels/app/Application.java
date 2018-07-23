3dd3472942f307b4dbf6983b8c943c50bd28b1ef
Alex Z <aleksey.zhd@gmail.com>
2018-07-23 22:15:53 +0700

    Задача 19. Выделил класс Application и перенёс в него методы

--- src/test/java/test_litecart/BasketTest_PageObject_3levels/app/Application.java
+++ src/test/java/test_litecart/BasketTest_PageObject_3levels/app/Application.java
@@ -1,4 +1,108 @@
 package test_litecart.BasketTest_PageObject_3levels.app;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
 public class Application {
+
+    // private WebDriverWait wait;
+    private WebDriver driver;
+
+    public Application() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver_win32.exe");
+        driver = new ChromeDriver();
+        // wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // задал неявное ожидание
+        driver.manage().window().maximize();
+    }
+
+    public void quit() {
+        driver.quit();
+        driver = null; // сам дописал
+    }
+
+    // Метод проверяющий есть ли элемент:
+    private boolean areElementsPresent(WebDriver driver, By locator){
+        return driver.findElements(locator).size() > 0;
+    }
+
+    // Метод для добавления 1 товара в корзину:
+    public void addOneProductToBasket(){
+        System.out.print("Открываю главную страницу магазина\n"); // добавил сюда
+        driver.get("http://localhost/litecart/en/"); // добавил сюда
+
+        System.out.print("\nНачало функции addOneProductToBasket\n");
+        System.out.print("Открываю первый товар из списка Most Popular\n");
+        driver.findElement(By.xpath("//div[@id='box-most-popular']//ul/li[1]")).click();
+        int currentAmount = Integer.parseInt(driver.findElement(By.xpath("//span[@class='quantity']")).getText());
+        int increasedAmount = currentAmount + 1;
+        System.out.print("currentAmount = " + currentAmount + "\n");
+        System.out.print("increasedAmount = " + increasedAmount + "\n");
+
+        // Для некоторых товаров необходимо указать размер:
+        if (areElementsPresent(driver, By.xpath("//select[@name='options[Size]']"))) {
+            driver.findElement(By.xpath("//select[@name='options[Size]']")).sendKeys("Small");
+        }
+
+        System.out.print("Жму кнопку для добавления в корзину\n");
+        driver.findElement(By.xpath("//button[@name='add_cart_product']")).click();
+
+        System.out.print("Жду пока счётчик товаров в корзине обновится... ");
+        // increasedAmount = 22; // для проверки падения теста
+        (new WebDriverWait(driver, 5)).until(ExpectedConditions.textToBePresentInElement(
+                By.xpath("//span[@class='quantity']"),
+                String.valueOf(increasedAmount)
+        ));
+        System.out.print("Счётчик обновлён!\n");
+
+        System.out.print("Возвращаюсь на главную страницу\n");
+        driver.findElement(By.xpath("//li[@class='general-0']")).click();
+        System.out.print("Конец функции addOneProductToBasket\n");
+    }
+
+    // Метод для удаления всех товаров из корзины:
+    public void deleteAllProductsFromBasket() {
+        System.out.print("\nНачало функции deleteAllProductsFromBasket\n");
+        System.out.print("Открываю корзину\n");
+        driver.findElement(By.xpath("//div[@id='cart']/a[@class='link']")).click();
+        int amountProductsInBasket = driver.findElements(By.xpath("//ul[@class='shortcuts']/li")).size();
+        System.out.print("Количество товаров в корзине = " + amountProductsInBasket + "\n");
+
+        for (int i=1; i<=amountProductsInBasket; i++){
+            int currentLines = driver.findElements(By.xpath("//table[@class='dataTable rounded-corners']/tbody/tr")).size();
+            int newLines = currentLines - 1;
+            System.out.print("Текущее количество строк в таблице: " + currentLines + "\n");
+            System.out.print("Удаляю один товар\n");
+            driver.findElement(By.xpath("//button[@name='remove_cart_item']")).click();
+
+            if (i<amountProductsInBasket){
+                System.out.print("В таблице осталось строк: " + newLines + "\n");
+                System.out.print("Жду пока количество строк в таблице обновится... ");
+                // newLines = 12; // для проверки падения теста
+                (new WebDriverWait(driver, 5)).until(ExpectedConditions.numberOfElementsToBe(
+                        By.xpath("//table[@class='dataTable rounded-corners']/tbody/tr"),
+                        newLines
+                ));
+                System.out.print("Таблица обновилась!\n");
+            }
+            else {
+                System.out.print("Жду появления сообщения о том что корзина пуста... ");
+                String expectedMessage = "There are no items in your cart.";
+                // expectedMessage = "Test Message"; // для проверки падения теста
+                (new WebDriverWait(driver, 5)).until(ExpectedConditions.textToBePresentInElement(
+                        By.xpath("//div[@id='checkout-cart-wrapper']//em"),
+                        expectedMessage
+                ));
+                System.out.print("Сообщение появилось!\n");
+            }
+        }
+        System.out.print("Конец функции deleteAllProductsFromBasket\n");
+
+        // Thread.sleep(5000); // использую для отладки. убрать потом !!!!!!!!
+    }
 }
--- src/test/java/test_litecart/BasketTest_PageObject_3levels/tests/TestBase.java
+++ src/test/java/test_litecart/BasketTest_PageObject_3levels/tests/TestBase.java
@@ -1,4 +1,38 @@
 package test_litecart.BasketTest_PageObject_3levels.tests;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import test_litecart.BasketTest_PageObject_3levels.app.Application;
+
+import java.util.concurrent.TimeUnit;
+
 public class TestBase {
+
+    // public static WebDriver driver;
+    public static Application app;
+
+    @BeforeClass
+    public static void start(){
+        System.out.print("\n\n***** Внутри метода start() *****\n\n");
+
+        app = new Application();
+
+        /*
+        System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver_win32.exe");
+        driver = new ChromeDriver(); // инициализация драйвера
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // задал неявное ожидание
+        driver.manage().window().maximize();
+        */
+    }
+
+    @AfterClass
+    public static void stop(){
+        System.out.print("\n\n***** Внутри метода stop() *****\n\n");
+
+        // driver.quit();
+        // driver = null;
+        app.quit();
+    }
 }
