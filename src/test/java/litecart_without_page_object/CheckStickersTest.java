7465c53601aa5cb37a95e5c35c4efa5b756b55c4
Alex Z <aleksey.zhd@gmail.com>
2018-08-01 23:49:07 +0700

    Переделал № 8 (без РО)

--- src/test/java/litecart_without_page_object/CheckStickersTest.java
+++ src/test/java/litecart_without_page_object/CheckStickersTest.java
@@ -1,76 +1,44 @@
 package litecart_without_page_object;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
-
-//import static com.sun.tools.doclint.Entity.or;
-
 public class CheckStickersTest extends TestBase {
-  // private static WebDriver driver;
 
-  /* @BeforeClass
-  public static void start() {
-    System.setProperty("webdriver.ie.driver", "C:\\Tools\\IEDriverServer_Win32_3.12.0.exe");
-    driver = new InternetExplorerDriver(); // инициализация драйвера
-    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // задал неявное ожидание
-    driver.manage().window().maximize();
-  } */
-
-  // 008 Тест, который проверяет наличие стикеров у всех товаров
   @Test
   public void test008() {
     System.out.print("\n\n***** Внутри метода test008() *****\n\n");
-    // ----------------------------------------------------
-    // Открыть главную страницу
-    // ----------------------------------------------------
+
+    System.out.println("Открываю главную страницу");
     driver.get("http://localhost/litecart/");
     (new WebDriverWait(driver, 3)).until(ExpectedConditions.titleIs("Online Store | My Store"));
-
-    // ----------------------------------------------------
-    // Найти все категории товаров.
-    // Просмотреть товары в каждой категории и убедиться,
-    // что у каждого товара имеется ровно 1 стикер
-    // ----------------------------------------------------
     int amountMerchandiseCategories = driver.findElements(By.xpath("//div[@class='middle']/div[@class='content']/div[@class='box']")).size();
     System.out.println("Количество категорий товаров = " + amountMerchandiseCategories);
-    for (int i = 0; i < amountMerchandiseCategories; i++) {
-      int iIncrement = i + 1;
-      String categoryTitle = driver.findElements(By.xpath("//div[@class='middle']/div[@class='content']/div[@class='box']/h3[@class='title']")).get(i).getText();
-      System.out.println("\nКАТЕГОРИЯ №" + iIncrement + "\nЗаголовок = " + categoryTitle);
 
+    for (int i = 0; i < amountMerchandiseCategories; i++) {
+      String categoryTitle = driver.findElements(By.xpath("//div[@class='middle']/div[@class='content']/div[@class='box']/h3[@class='title']")).get(i).getText();
+      System.out.println("\nКАТЕГОРИЯ №" + (i+1) + "\nЗаголовок = " + categoryTitle);
       WebElement merchandiseCategory = driver.findElements(By.xpath("//div[@class='middle']/div[@class='content']/div[@class='box']")).get(i);
       int amountMerchandiseInCategory = merchandiseCategory.findElements(By.xpath(".//ul[@class='listing-wrapper products']/li")).size();
       System.out.println("Количество товаров в текущей категории = " + amountMerchandiseInCategory);
-      for (int j = 0; j < amountMerchandiseInCategory; j++) {
-        int jIncrement = j + 1;
-        System.out.print("Категория/товар: " + iIncrement + "/" + jIncrement + ", ");
 
+      for (int j = 0; j < amountMerchandiseInCategory; j++) {
+        System.out.print("Категория/товар: " + (i+1) + "/" + (j+1) + ", ");
         merchandiseCategory = driver.findElements(By.xpath("//div[@class='middle']/div[@class='content']/div[@class='box']")).get(i);
-        String merchandiseName = merchandiseCategory.findElement(By.xpath(".//ul[@class='listing-wrapper products']/li[" + jIncrement + "]//div[@class='name']")).getText();
+        String merchandiseName = merchandiseCategory.findElement(By.xpath(".//ul[@class='listing-wrapper products']/li[" + (j+1) + "]//div[@class='name']")).getText();
         System.out.print("название = " + merchandiseName + ". ");
 
-        System.out.print("Проверяю что стикер один... (или ноль) ");
+        System.out.print("Проверяю что стикеров один или ноль: ");
         merchandiseCategory = driver.findElements(By.xpath("//div[@class='middle']/div[@class='content']/div[@class='box']")).get(i);
-        int stickerAmount = merchandiseCategory.findElements(By.xpath(".//ul[@class='listing-wrapper products']/li[" + jIncrement + "]//div[contains(@class,'sticker')]")).size();
-        System.out.println("Количество стикеров = " + stickerAmount);
+        int stickerAmount = merchandiseCategory.findElements(By.xpath(".//ul[@class='listing-wrapper products']/li[" + (j+1) + "]//div[contains(@class,'sticker')]")).size();
+        // stickerAmount = 3; // для проверки падения теста
+        System.out.println("количество стикеров = " + stickerAmount);
         Assert.assertTrue(stickerAmount==0 || stickerAmount==1);
       }
     }
   }
-
- /*@AfterClass
-  public static void stop() {
-    // driver.quit();
-    // driver = null;
-  } */
 }
