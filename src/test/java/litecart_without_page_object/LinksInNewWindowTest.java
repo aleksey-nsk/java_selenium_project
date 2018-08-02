13c0f5e5995cd1a69d3809b6979ecfc16d6590b9
Alex Z <aleksey.zhd@gmail.com>
2018-08-02 22:14:00 +0700

    Переделал № 14 (без РО)

--- src/test/java/litecart_without_page_object/LinksInNewWindowTest.java
+++ src/test/java/litecart_without_page_object/LinksInNewWindowTest.java
@@ -1,44 +1,17 @@
-/*
-Описание теста:
-
-Задание 14. Проверьте, что ссылки открываются в новом окне.
-Сделайте сценарий, который проверяет, что ссылки на странице
-редактирования страны открываются в новом окне.
-
-1) зайти в админку.
-2) открыть пункт меню Countries.
-3) открыть на редактирование какую-нибудь страну.
-4) возле некоторых полей есть ссылки с иконкой
-в виде квадратика со стрелкой - они ведут на внешние страницы
-и открываются в новом окне, именно это и нужно проверить.
-
-Конечно, можно просто убедиться в том, что у ссылки есть атрибут target="_blank".
-Но в этом упражнении требуется именно кликнуть по ссылке, чтобы она открылась
-в новом окне, потом переключиться в новое окно, закрыть его, вернуться обратно,
-и повторить эти действия для всех таких ссылок. Не забудьте, что новое окно
-открывается не мгновенно, поэтому требуется ожидание открытия окна.
-*/
-
 package litecart_without_page_object;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
-public class LinksInNewWindowTest {
-  private static WebDriver driver;
+public class LinksInNewWindowTest extends TestBase {
 
-  // Ожидание появления нового окна:
   public ExpectedCondition<String> anyWindowOtherThan(Set<String> oldWindows) {
+    System.out.println("Метод для ожидания появления нового окна");
     return new ExpectedCondition<String>() {
       @Override
       public String apply(WebDriver driver) {
@@ -49,103 +22,75 @@ public class LinksInNewWindowTest {
     };
   }
 
-  @BeforeClass
-  public static void start() {
-    System.out.print("\n\n***** Внутри метода start() *****\n\n");
-
-    System.setProperty("webdriver.ie.driver", "C:\\Tools\\IEDriverServer_Win32_3.12.0.exe");
-    driver = new InternetExplorerDriver(); // инициализация драйвера
-
-    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // задал неявное ожидание
-    driver.manage().window().maximize();
-  }
-
   @Test
-  public void action() throws InterruptedException {
-    System.out.print("\n\n***** Внутри метода action() *****\n\n");
+  public void test014() throws InterruptedException {
+    System.out.print("\n\n***** Внутри метода test014() *****\n\n");
 
-    System.out.print("Захожу в панель администрирования\n");
-    driver.get("http://localhost/litecart/admin/");
-    driver.findElement(By.xpath("//input[@name='username']")).sendKeys("admin");
-    driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin");
-    driver.findElement(By.xpath("//button[@name='login']")).click();
-    String accountUser = driver.findElement(By.xpath("//div[contains(@class,'notice success')]")).getText();
-    Assert.assertEquals("You are now logged in as admin", accountUser);
+    goToAdminPanel();
 
-    System.out.print("Захожу в раздел Countries\n");
+    System.out.println("Захожу в раздел Countries");
     driver.findElement(By.xpath("//ul[@id='box-apps-menu']/li[3]")).click();
-
-    // System.out.print("Например, открою страницу редактирования Албании\n");
-    // driver.findElement(By.xpath("//form[@name='countries_form']//tbody/tr[3]/td[7]/a")).click();
-    System.out.print("Например, открою страницу редактирования Бразилии\n");
+    System.out.println("Например, открою страницу редактирования Бразилии");
     driver.findElement(By.xpath("//form[@name='countries_form']//tbody/tr[31]/td[7]/a")).click();
-
-    System.out.print("Заголовок страницы редактирования = ");
     String originalTitle = driver.getTitle();
-    System.out.print(originalTitle + "\n");
-
-    System.out.print("Ищу ссылки с иконкой в виде квадратика со стрелкой...\n");
+    System.out.println("Заголовок страницы редактирования = " + originalTitle);
+    System.out.println("\nИщу ссылки с иконкой в виде квадратика со стрелкой");
     int amountOfExternalLinks = driver.findElements(By.xpath("//i[@class='fa fa-external-link']")).size();
-    System.out.print("  всего таких ссылок: " + amountOfExternalLinks + "\n");
-    System.out.print("  убедимся что все эти ссылки открываются в новом окне\n");
+    System.out.println("  всего таких ссылок: " + amountOfExternalLinks);
+    System.out.println("  убедимся что все эти ссылки открываются в новом окне");
 
     for (int i = 0; i < amountOfExternalLinks; i++) {
-      System.out.print("\nПроверяю текущую ссылку номер: " + (i + 1) + " ...\n");
+      System.out.println("\nПроверяю текущую ссылку номер: " + (i+1) );
 
       System.out.print("  запоминаю идентификатор текущего окна: ");
       String currentWindow = driver.getWindowHandle();
-      System.out.print("currentWindow = " + currentWindow + "\n");
+      System.out.println("currentWindow = " + currentWindow);
 
       System.out.print("  запоминаю идентификаторы уже открытых окон: ");
       Set<String> existingWindows = driver.getWindowHandles();
-      System.out.print("existingWindows = " + existingWindows + "\n");
+      System.out.println("existingWindows = " + existingWindows);
 
-      System.out.print("  кликаю ссылку которая открывает новое окно\n");
+      System.out.println("  кликаю ссылку которая открывает новое окно");
       driver.findElements(By.xpath("//i[@class='fa fa-external-link']")).get(i).click();
 
-      System.out.print("  жду появления нового окна с новым идентификатором...\n");
+      System.out.print("  жду появления нового окна с новым идентификатором...\n    ");
       String newWindow = (new WebDriverWait(driver, 5)).until(anyWindowOtherThan(existingWindows));
-      System.out.print("    появилось! newWindow = " + newWindow + "\n");
+      System.out.println("    появилось! newWindow = " + newWindow);
 
-      System.out.print("  переключаюсь в новое окно\n");
+      System.out.println("  переключаюсь в новое окно");
       driver.switchTo().window(newWindow);
 
-      System.out.print("  убедимся что переключились куда надо...\n");
+      System.out.println("  убедимся что переключились куда надо...");
       Set<String> newExistingWindows = driver.getWindowHandles();
-      System.out.print("    newExistingWindows = " + newExistingWindows + "\n");
+      System.out.println("    newExistingWindows = " + newExistingWindows);
       String newCurrentWindow = driver.getWindowHandle();
       // newCurrentWindow = "CDwindow-00112233"; // для проверки падения теста
-      System.out.print("    newCurrentWindow = " + newCurrentWindow + "\n");
+      System.out.println("    newCurrentWindow = " + newCurrentWindow);
       Assert.assertEquals(newWindow, newCurrentWindow);
-      System.out.print("    Верно!\n");
+      // Хочу немного задержаться в открытом окне:
+      if (isElementPresent(driver, By.xpath("//div[@id='thisElementIsNotExist']"))) {
+        System.out.println("Эту надпись мы никогда не увидим!");
+      }
 
-      System.out.print("  закрываю это новое окно\n");
+      System.out.println("  закрываю это новое окно");
       driver.close();
 
-      System.out.print("  возвращаюсь в исходное окно\n");
+      System.out.println("  возвращаюсь в исходное окно");
       driver.switchTo().window(currentWindow);
 
-      System.out.print("  проверю заголовок страницы...\n");
+      System.out.println("  проверю заголовок страницы...");
       String finalTitle = driver.getTitle();
       // finalTitle = "Test Title"; // для проверки падения теста
-      System.out.print("    ОР = " + originalTitle + "\n    ФР = " + finalTitle + "\n");
+      System.out.println("    ОР = " + originalTitle);
+      System.out.println("    ФР = " + finalTitle);
       Assert.assertEquals(originalTitle, finalTitle);
-      System.out.print("    Верно!\n");
 
       // if (i == 1) break; // использую для отладки
     }
 
-    System.out.print("\nНичего редактировать не буду. Жму кнопку Cancel\n");
+    System.out.println("\nНичего редактировать не буду. Жму кнопку Cancel");
     driver.findElement(By.xpath("//button[@name='cancel']")).click();
-    System.out.print("Выхожу из панели администрирования\n");
-    driver.findElement(By.xpath("//a[@title='Logout']")).click();
-  }
 
-  @AfterClass
-  public static void stop() {
-    System.out.print("\n\n***** Внутри метода stop() *****\n\n");
-
-    driver.quit();
-    driver = null;
+    exitAdminPanel();
   }
 }
