16d0b1d7eb136a2db741d1ae5390b597a08e4214
Alex Z <aleksey.zhd@gmail.com>
2018-08-04 22:38:26 +0700

    Подредактировал nds_mail ---> OpenUrlTest

--- src/test/java/ngs_mail/OpenUrlTest.java
+++ src/test/java/ngs_mail/OpenUrlTest.java
@@ -1,8 +1,7 @@
 /*
-В данном тесте использую явные ожидания.
 Описание теста:
 - открываю страницу почты ngs
-- жду пока загрузится
+- жду пока загрузится (использую явные ожидания)
 - закрываю браузер
 */
 
@@ -13,37 +12,31 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class OpenUrlTest {
+
   private static WebDriver driver;
 
   @BeforeClass
   public static void start() {
-    /*
-    System.setProperty("webdriver.ie.driver", "C:\\Tools\\IEDriverServer_Win32_3.12.0.exe");
-    driver = new InternetExplorerDriver(); // инициализация драйвера
-    driver.manage().window().maximize();
-    */
-    // Проверю работу автотеста в 3 основных браузерах:
+    System.out.print("\n\n***** Внутри метода start() *****\n\n");
     System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver_win32.exe");
     driver = new ChromeDriver(); // инициализация драйвера
-    // System.setProperty("webdriver.ie.driver", "C:\\Tools\\IEDriverServer_Win32_3.12.0.exe");
-    // driver = new InternetExplorerDriver(); // инициализация драйвера
-    // System.setProperty("webdriver.gecko.driver", "C:\\Tools\\geckodriver-v0.20.1-win64.exe");
-    // driver = new FirefoxDriver(); // инициализация драйвера
   }
 
   @Test
-  public void action() {
+  public void openUrlTest() {
+    System.out.print("\n\n***** Внутри метода openUrlTest() *****\n\n");
+    System.out.println("Открываю страницу почты ngs");
     driver.get("https://mail.ngs.ru/");
     (new WebDriverWait(driver, 5)).until(ExpectedConditions.titleIs("ГОРОДСКАЯ ПОЧТОВАЯ СЛУЖБА"));
   }
 
   @AfterClass
   public static void stop() {
+    System.out.print("\n\n***** Внутри метода stop() *****\n\n");
     driver.quit();
     driver = null;
   }
