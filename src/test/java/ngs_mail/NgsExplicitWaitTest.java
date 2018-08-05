4373b203a39a1379e73b82d2c9b416acb38286f9
Alex Z <aleksey.zhd@gmail.com>
2018-08-05 13:00:29 +0700

    добавил проверку в 3 основных браузерах в тест NgsExplicitWaitTest

--- src/test/java/ngs_mail/NgsExplicitWaitTest.java
+++ src/test/java/ngs_mail/NgsExplicitWaitTest.java
@@ -16,6 +16,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -26,8 +28,16 @@ public class NgsExplicitWaitTest {
   @BeforeClass
   public static void start() {
     System.out.print("\n\n***** Внутри метода start() *****\n\n");
-    System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver_win32.exe");
-    driver = new ChromeDriver(); // инициализация драйвера
+
+    // Проверю работу автотеста в 3 основных браузерах:
+    // System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver_win32.exe");
+    // driver = new ChromeDriver(); // инициализация драйвера
+    // System.setProperty("webdriver.ie.driver", "C:\\Tools\\IEDriverServer_Win32_3.12.0.exe");
+    // driver = new InternetExplorerDriver(); // инициализация драйвера
+    System.setProperty("webdriver.gecko.driver", "C:\\Tools\\geckodriver-v0.20.1-win64.exe");
+    driver = new FirefoxDriver(); // инициализация драйвера
+
+    driver.manage().window().maximize();
   }
 
   @Test
