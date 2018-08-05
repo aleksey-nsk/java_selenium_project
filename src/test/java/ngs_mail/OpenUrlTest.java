5296cc8a42bc3aa32b95f94d2f251c1aa772215b
Alex Z <aleksey.zhd@gmail.com>
2018-08-05 15:03:28 +0700

    добавил проверку в 3 основных браузерах в тест openUrlTest

--- src/test/java/ngs_mail/OpenUrlTest.java
+++ src/test/java/ngs_mail/OpenUrlTest.java
@@ -12,32 +12,56 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class OpenUrlTest {
 
-  private static WebDriver driver;
+  private static WebDriver driverChrome, driverIE, driverFirefox;
+  private static WebDriver[] drivers = new WebDriver[3];
 
   @BeforeClass
   public static void start() {
     System.out.print("\n\n***** Внутри метода start() *****\n\n");
+
+    // Проверю работу автотеста в 3 основных браузерах:
     System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver_win32.exe");
-    driver = new ChromeDriver(); // инициализация драйвера
+    driverChrome = new ChromeDriver(); // инициализация драйвера
+    System.out.println("====================================");
+    System.setProperty("webdriver.ie.driver", "C:\\Tools\\IEDriverServer_Win32_3.12.0.exe");
+    driverIE = new InternetExplorerDriver(); // инициализация драйвера
+    System.out.println("====================================");
+    System.setProperty("webdriver.gecko.driver", "C:\\Tools\\geckodriver-v0.20.1-win64.exe");
+    driverFirefox = new FirefoxDriver(); // инициализация драйвера
+
+    drivers[0] = driverChrome;
+    drivers[1] = driverIE;
+    drivers[2] = driverFirefox;
   }
 
   @Test
   public void openUrlTest() {
     System.out.print("\n\n***** Внутри метода openUrlTest() *****\n\n");
-    System.out.println("Открываю страницу почты ngs");
-    driver.get("https://mail.ngs.ru/");
-    (new WebDriverWait(driver, 5)).until(ExpectedConditions.titleIs("ГОРОДСКАЯ ПОЧТОВАЯ СЛУЖБА"));
+
+    for (int i=0; i<3; i++) {
+      System.out.println("Открываю страницу почты ngs. Использую драйвер:");
+      System.out.print(drivers[i] + "\n\n");
+      drivers[i].get("https://mail.ngs.ru/");
+      (new WebDriverWait(drivers[i], 5)).until(ExpectedConditions.titleIs("ГОРОДСКАЯ ПОЧТОВАЯ СЛУЖБА"));
+    }
   }
 
   @AfterClass
   public static void stop() {
     System.out.print("\n\n***** Внутри метода stop() *****\n\n");
-    driver.quit();
-    driver = null;
+
+    for (int i=0; i<3; i++) {
+      System.out.println("Останавливаю драйвер:");
+      System.out.print(drivers[i] + "\n\n");
+      drivers[i].quit();
+      drivers[i] = null;
+    }
   }
 }
